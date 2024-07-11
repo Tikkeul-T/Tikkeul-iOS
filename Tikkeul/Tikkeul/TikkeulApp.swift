e9225cca69429bc58914156d9d15272b7029792b
정지훈 <jihun332@gmail.com>
2024-07-11 17:46:21 +0900

    🎉 티끌 프로젝트 생성

---
+++ Tikkeul/Tikkeul/TikkeulApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikkeulApp.swift
+//  Tikkeul
+//
+//  Created by 정지훈 on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TikkeulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
